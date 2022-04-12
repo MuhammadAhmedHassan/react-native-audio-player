234068b054c9bba8eaeddc6cdd48e676fc9fc632
Muhammad Ahmed Hassan <ah363702@gmail.com>
2022-04-12 11:03:02 +0500

    initial commit

---
+++ model/data.ts
@@ -0,0 +1,32 @@
+export const songs = [
+  {
+    title: 'Kontho',
+    artist: 'Anupamtt Royuit',
+    image: require('../assets/artworks/image-1.jpg'),
+    id: '1',
+  },
+  {
+    title: 'Humraah',
+    artist: 'Adchn dkigne',
+    image: require('../assets/artworks/image-2.jpg'),
+    id: '2',
+  },
+  {
+    title: 'Kyon',
+    artist: 'AD Kin KUO',
+    image: require('../assets/artworks/image-3.jpg'),
+    id: '3',
+  },
+  {
+    title: 'Buzz',
+    artist: 'L diKI Xi',
+    image: require('../assets/artworks/image-4.jpg'),
+    id: '4',
+  },
+  {
+    title: 'The Train',
+    artist: 'LINKed IN',
+    image: require('../assets/artworks/image-5.jpg'),
+    id: '5',
+  },
+]
