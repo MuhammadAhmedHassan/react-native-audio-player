ddfddf69e6151ebefa4decc6cb0d07b69d76324a
Muhammad Ahmed Hassan <ah363702@gmail.com>
2022-04-14 12:46:27 +0500

    add link of article in index.js

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ import App from './App';
 import {name as appName} from './app.json';
 import TrackPlayer from 'react-native-track-player';
 
+// Add link of article
+// https://medium.com/@bharat.tiwari/creating-an-audio-player-in-react-native-2628c4262db4
+
 AppRegistry.registerComponent(appName, () => App);
 // Might work during production
 // TrackPlayer.registerPlaybackService(() => require('./build/utilsAndServices/TrackPlayerService'));
--- utilsAndServices/TrackPlayerSerivce.ts
+++ utilsAndServices/TrackPlayerSerivce.ts
@@ -3,7 +3,16 @@ import TrackPlayer, {Event} from 'react-native-track-player';
 module.exports = async function () {
   // This service needs to be registered for the module to work
   // but it will be used later in the "Receiving Events" section
-  TrackPlayer.addEventListener(Event.RemotePlay, () => TrackPlayer.play());
-  TrackPlayer.addEventListener(Event.RemotePause, () => TrackPlayer.pause());
-  TrackPlayer.addEventListener(Event.RemoteStop, () => TrackPlayer.destroy());
+  TrackPlayer.addEventListener(
+    Event.RemotePlay,
+    async () => await TrackPlayer.play(),
+  );
+  TrackPlayer.addEventListener(
+    Event.RemotePause,
+    async () => await TrackPlayer.pause(),
+  );
+  TrackPlayer.addEventListener(
+    Event.RemoteStop,
+    async () => await TrackPlayer.destroy(),
+  );
 };
